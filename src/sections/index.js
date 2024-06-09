60c6631f13faae726cadd904f8fcb888c6816bf9
Gaurav-bajpaye <gauravbajpaye@gmail.com>
2024-06-09 13:13:52 +0530

    complet commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffers from "./SpecialOffers";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+
+export {
+   Hero,
+   PopularProducts,
+   Subscribe,
+   SuperQuality,
+   Footer,
+   CustomerReviews,
+   Services,
+   SpecialOffers
+
+}
